4736d49cc5c7988a0669df5e34be0df8a7e9368d
Suke0811 <49264928+Suke0811@users.noreply.github.com>
2025-06-09 11:59:04 -0700

    Update: Add support for Python 3.12 and 3.13 in package_info.py

--- setup/package_info.py
+++ setup/package_info.py
@@ -7,18 +7,18 @@ REPOSITORY_NAME = 'fspin'
 
 PYTHON_REQUIRES = '>=3.7, <4.0'  # Specify Python version compatibility
 CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
     'Framework :: Robot Framework',
     'Intended Audience :: Developers',
     'Intended Audience :: Education',
     'Intended Audience :: Science/Research',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
 ]
 
 ######################################
